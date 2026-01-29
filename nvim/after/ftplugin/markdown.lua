0431331d4ca835fc4ef969ee0631ba7db6f23910
Abdallah Meebed <abdallah.meebed@univ-grenoble-alpes.fr>
2026-01-29 14:24:39 +0100

    Update nvim and add tmux config

--- nvim/after/ftplugin/markdown.lua
+++ nvim/after/ftplugin/markdown.lua
@@ -1,2 +1,3 @@
 vim.wo[0][0].spell = true
 vim.opt.textwidth = 100
+vim.wo[0][0].colorcolumn = "+1"
--- nvim/after/ftplugin/typst.lua
+++ nvim/after/ftplugin/typst.lua
@@ -3,3 +3,4 @@ vim.opt.textwidth = 100
 vim.wo[0][0].foldlevel = 1
 vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
 vim.wo[0][0].foldmethod = 'expr'
+vim.wo[0][0].colorcolumn = "+1"
