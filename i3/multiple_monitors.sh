3d12f1e5a506ad4758aeca9029e8223c563734d9
Abdallah Meebed <abdallah.meebed@univ-grenoble-alpes.fr>
2026-03-23 14:33:16 +0100

    Update i3 config, alacritty and remove redundant nvim options

--- i3/multiple_monitors.sh
+++ i3/multiple_monitors.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/sh
 
-monitors=`xrandr | grep "\bconnected\b" | grep -oE '^[^ ]+'`
+monitors=`xrandr | grep -w "connected" | grep -oE '^[^ ]+'`
 
 if [ $((`echo "$monitors" | wc -l`)) -eq 3 ]; then
     xrandr --output eDP-1 --off --output DP-2-1-8 --left-of DP-2-8
